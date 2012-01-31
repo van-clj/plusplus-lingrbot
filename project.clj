fc14739d217546b7f81f0ccc0f6492c1b81b11e5
ujihisa <ujihisa@gmail.com>
2012-01-30 20:51:48 -0800

    prototype: just echos.

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject lingr-plusplus-bot "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :main lingr-plusplus-bot.core
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.2"]
+                 [compojure "1.0.0-RC2"]
+                 [ring "1.0.1"]])
--- src/lingr_plusplus_bot/core.clj
+++ src/lingr_plusplus_bot/core.clj
@@ -1,1 +1,16 @@
-(ns lingr-plusplus-bot.core)
+(ns lingr-plusplus-bot.core
+  (:use
+    [clojure.data.json :only (read-json)]
+    [compojure.core]
+    [ring.adapter.jetty]))
+
+(defroutes
+  hello
+  (GET "/" [] "plus plus")
+  (POST "/"
+        {body :body}
+        (let [message (:message (first (:events (read-json (slurp body)))))]
+          (str (:text message) "++"))))
+
+(defn -main []
+  (run-jetty hello {:port 4003}))
