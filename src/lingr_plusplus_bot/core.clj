2e0f2022ce9261f1a5e4b1b1b5449411ad25e195
ujihisa <ujihisa@gmail.com>
2012-02-03 08:39:15 -0800

    combines them with "\n"

--- src/lingr_plusplus_bot/core.clj
+++ src/lingr_plusplus_bot/core.clj
@@ -12,7 +12,7 @@
   (GET "/" [] "plus plus")
   (POST "/"
         {body :body}
-        (map (fn [event]
+        (apply str (interpose "\n" (map (fn [event]
           (let [message (:message event)
                 plus (second (re-find #"([a-zA-Z0-9_-]+)\+\+$" (:text message)))
                 minus (second (re-find #"([a-zA-Z0-9_-]+)--$" (:text message)))
@@ -21,17 +21,17 @@
             (cond
               plus (let [cnt (+ (or (get @plusplus plus) 0) 1)]
                      (swap! plusplus assoc plus cnt)
-                     (str plus "++ (" cnt ") "))
+                     (str plus "++ (" cnt ")"))
               minus (let [cnt (+ (or (get @plusplus minus) 0) -1)]
                         (swap! plusplus assoc minus cnt)
-                        (str minus "-- (" cnt ") "))
+                        (str minus "-- (" cnt ")"))
               pluseq (let [cnt (+ (get @plusplus pluseq) 0)]
                          (swap! plusplus assoc pluseq cnt)
-                         (str pluseq "+=" pluseq "(" cnt ") "))
+                         (str pluseq "+=" pluseq "(" cnt ")"))
               minuseq (let [cnt (- (get @plusplus minuseq) 0)]
                          (swap! plusplus assoc minuseq cnt)
-                         (str minuseq "-=" minuseq "(" cnt ") "))
-              :else ""))) (:events (read-json (slurp body))))))
+                         (str minuseq "-=" minuseq "(" cnt ")"))
+              :else ""))) (:events (read-json (slurp body))))))))
 
 (defn -main []
   (with-open [r (reader "plusplus.json")]
