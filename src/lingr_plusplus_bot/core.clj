9162ac84e72d031338e0d3d7dea6998665047479
ujihisa <ujihisa@gmail.com>
2012-02-03 00:39:02 -0800

    if -> cond

--- src/lingr_plusplus_bot/core.clj
+++ src/lingr_plusplus_bot/core.clj
@@ -17,20 +17,20 @@
               minus (second (re-find #"([a-zA-Z0-9_-]+)--$" (:text message)))
               pluseq (nth (re-find #"([a-zA-Z0-9_-]+)\+=(0-9)$" (:text message)) 3)
               minuseq (nth (re-find #"([a-zA-Z0-9_-]+)-=(0-9)$" (:text message)) 3)]
-          (if plus
-            (let [cnt (+ (or (get @plusplus plus) 0) 1)]
-              (swap! plusplus assoc plus cnt)
-              (str plus "++ (" cnt ")"))
-            (if minus
-              (let [cnt (+ (or (get @plusplus minus) 0) -1)]
-                (swap! plusplus assoc minus cnt)
-                (str minus "-- (" cnt ")"))
-              "")
-            (if pluseq 
-              (let [cnt (+ (get @plusplus pluseq) 0)] 
-                (swap! plusplus assoc pluseq cnt)
-                (str pluseq "+=" pluseq "(" cnt ")"))
-              "")
+          (cond
+            plus (let [cnt (+ (or (get @plusplus plus) 0) 1)]
+                   (swap! plusplus assoc plus cnt)
+                   (str plus "++ (" cnt ")"))
+            minus (do
+                    (let [cnt (+ (or (get @plusplus minus) 0) -1)]
+                      (swap! plusplus assoc minus cnt)
+                      (str minus "-- (" cnt ")"))
+                    "")
+            pluseq (do
+                     (let [cnt (+ (get @plusplus pluseq) 0)] 
+                       (swap! plusplus assoc pluseq cnt)
+                       (str pluseq "+=" pluseq "(" cnt ")"))
+                     ""))
             (if pluseq
               (let [cnt (+ (get @plusplus pluseq) 0)]
                 (swap! plusplus assoc pluseq cnt)
