a3de94c1b3ce774eeff3fa82415f5d4e837445df
mattn <mattn.jp@gmail.com>
2012-02-03 19:44:07 +0900

    do for events.

--- src/lingr_plusplus_bot/core.clj
+++ src/lingr_plusplus_bot/core.clj
@@ -12,25 +12,26 @@
   (GET "/" [] "plus plus")
   (POST "/"
         {body :body}
-        (let [message (:message (first (:events (read-json (slurp body)))))
-              plus (second (re-find #"([a-zA-Z0-9_-]+)\+\+$" (:text message)))
-              minus (second (re-find #"([a-zA-Z0-9_-]+)--$" (:text message)))
-              pluseq (nth (re-find #"([a-zA-Z0-9_-]+)\+=([0-9])$" (:text message)) 3)
-              minuseq (nth (re-find #"([a-zA-Z0-9_-]+)-=([0-9])$" (:text message)) 3)]
-          (cond
-            plus (let [cnt (+ (or (get @plusplus plus) 0) 1)]
-                   (swap! plusplus assoc plus cnt)
-                   (str plus "++ (" cnt ")"))
-            minus (let [cnt (+ (or (get @plusplus minus) 0) -1)]
-                      (swap! plusplus assoc minus cnt)
-                      (str minus "-- (" cnt ")"))
-            pluseq (let [cnt (+ (get @plusplus pluseq) 0)]
-                       (swap! plusplus assoc pluseq cnt)
-                       (str pluseq "+=" pluseq "(" cnt ")"))
-            minuseq (let [cnt (- (get @plusplus minuseq) 0)]
-                       (swap! plusplus assoc minuseq cnt)
-                       (str minuseq "-=" minuseq "(" cnt ")"))
-            :else ""))))
+        (map (fn [event]
+          (let [message (:message event)
+                plus (second (re-find #"([a-zA-Z0-9_-]+)\+\+$" (:text message)))
+                minus (second (re-find #"([a-zA-Z0-9_-]+)--$" (:text message)))
+                pluseq (nth (re-find #"([a-zA-Z0-9_-]+)\+=([0-9])$" (:text message)) 3)
+                minuseq (nth (re-find #"([a-zA-Z0-9_-]+)-=([0-9])$" (:text message)) 3)]
+            (cond
+              plus (let [cnt (+ (or (get @plusplus plus) 0) 1)]
+                     (swap! plusplus assoc plus cnt)
+                     (str plus "++ (" cnt ")"))
+              minus (let [cnt (+ (or (get @plusplus minus) 0) -1)]
+                        (swap! plusplus assoc minus cnt)
+                        (str minus "-- (" cnt ")"))
+              pluseq (let [cnt (+ (get @plusplus pluseq) 0)]
+                         (swap! plusplus assoc pluseq cnt)
+                         (str pluseq "+=" pluseq "(" cnt ")"))
+              minuseq (let [cnt (- (get @plusplus minuseq) 0)]
+                         (swap! plusplus assoc minuseq cnt)
+                         (str minuseq "-=" minuseq "(" cnt ")"))
+              :else ""))) (:events (read-json (slurp body))))))
 
 (defn -main []
   (with-open [r (reader "plusplus.json")]
