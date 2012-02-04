b54ecabedca8b2965f79ae58abe05d72a8df5af3
mattn <mattn.jp@gmail.com>
2012-02-05 04:15:22 +0900

    add test.

--- test/lingr_plusplus_bot/test/core.clj
+++ test/lingr_plusplus_bot/test/core.clj
@@ -1,6 +1,35 @@
 (ns lingr-plusplus-bot.test.core
   (:use [lingr-plusplus-bot.core])
+  (:use [clojure.string :only (trim)])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-plusplus
+  (swap! plusplus assoc "thinca" 0)
+  (swap! plusplus assoc "mattn" 0)
+  (is (= (trim (event2response {:message {:text "mattn++"}})) "mattn++ (1)"))
+  (is (= (trim (event2response {:message {:text "thinca++"}})) "thinca++ (1)"))
+  (is (= (trim (event2response {:message {:text "mattn++"}})) "mattn++ (2)"))
+)
+
+(deftest test-minusminus
+  (swap! plusplus assoc "thinca" 3)
+  (swap! plusplus assoc "mattn" 2)
+  (is (= (trim (event2response {:message {:text "mattn--"}})) "mattn-- (1)"))
+  (is (= (trim (event2response {:message {:text "thinca--"}})) "thinca-- (2)"))
+  (is (= (trim (event2response {:message {:text "mattn--"}})) "mattn-- (0)"))
+)
+
+(deftest test-plusequal
+  (swap! plusplus assoc "ujihisa" 20000)
+  (swap! plusplus assoc "mattn" 0)
+  (is (= (trim (event2response {:message {:text "mattn+=3"}})) "mattn+=3 (3)"))
+  (is (= (trim (event2response {:message {:text "ujihisa+=5"}})) "ujihisa+=5 (20005)"))
+)
+
+(deftest test-minusequal
+  (swap! plusplus assoc "ujihisa" 20000)
+  (swap! plusplus assoc "mattn" 0)
+  (is (= (trim (event2response {:message {:text "mattn-=3"}})) "mattn-=3 (-3)"))
+  (is (= (trim (event2response {:message {:text "ujihisa-=20000"}})) ""))
+  (is (= (trim (event2response {:message {:text "ujihisa-=2"}})) "ujihisa-=2 (19998)"))
+)
