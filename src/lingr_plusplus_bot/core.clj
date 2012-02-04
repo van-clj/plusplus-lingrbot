3e09e81562b1536b49e7a71ab5f1796b0223dabb
mattn <mattn.jp@gmail.com>
2012-02-05 04:14:44 +0900

    trimming spaces.

--- src/lingr_plusplus_bot/core.clj
+++ src/lingr_plusplus_bot/core.clj
@@ -24,11 +24,11 @@
       pluseq (let [cnt (+ (or (get @plusplus (nth pluseq 1)) 0)
                           (Integer/parseInt (nth pluseq 2)))]
                  (swap! plusplus assoc (nth pluseq 1) cnt)
-                 (str (nth pluseq 1) "+=" (nth pluseq 2) "(" cnt ") "))
+                 (str (nth pluseq 1) "+=" (nth pluseq 2) " (" cnt ")"))
       minuseq (let [cnt (- (or (get @plusplus (nth minuseq 1)) 0)
                            (Integer/parseInt (nth minuseq 2)))]
                  (swap! plusplus assoc (nth minuseq 1) cnt)
-                 (str (nth minuseq 1) "-=" (nth minuseq 2) "(" cnt ") "))
+                 (str (nth minuseq 1) "-=" (nth minuseq 2) " (" cnt ")"))
       :else "")))
 
 (defroutes
