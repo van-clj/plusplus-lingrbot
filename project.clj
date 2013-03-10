bef9051dd7ee0727d6c2541b6d4ad56b3dae3505
ujihisa <ujihisa@gmail.com>
2013-03-10 00:23:28 -0800

    1.1.0 development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lingr-plusplus-bot "1.0.0"
+(defproject lingr-plusplus-bot "1.1.0-SNAPSHOT"
   :description "increment your friend"
   :main lingr-plusplus-bot.core
   :dependencies [[org.clojure/clojure "1.5.0"]
