e068c185b4de757cdd46704dd201ed709da0cde2
ujihisa <ujihisa@gmail.com>
2013-03-14 21:38:55 -0700

    clojure version bump

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lingr-plusplus-bot "1.1.0-SNAPSHOT"
   :description "increment your friend"
   :main lingr-plusplus-bot.core
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.1"]
                  [compojure "1.1.5"]
                  [ring "1.1.8"]])
